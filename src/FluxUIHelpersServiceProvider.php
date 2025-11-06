380820c06f82d2a327ceacc322981c4b34388a03
TobiasNagel01 <120384605+TobiasNagel01@users.noreply.github.com>
2025-11-06 22:13:32 +0000

    Fix styling

--- src/FluxUIHelpersServiceProvider.php
+++ src/FluxUIHelpersServiceProvider.php
@@ -11,7 +11,7 @@ class FluxUIHelpersServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        $package->name('fluxuihelpers')->hasConfigFile('flux_ui_helpers');;
+        $package->name('fluxuihelpers')->hasConfigFile('flux_ui_helpers');
 
         Livewire::component('session-toast', SessionToast::class);
     }
